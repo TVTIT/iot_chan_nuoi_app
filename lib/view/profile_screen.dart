3631573e080ae091b91f13671a9ed9b9482ace72
TVTIT <trungtran200656@gmail.com>
2026-03-04 17:57:08 +0700

    hiện tên phiên bản ở profile screen và thay đổi tên phiên bản thành 0.0.1 build 1

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,25 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class MyHomeScreen extends StatelessWidget {
   const MyHomeScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: const Text('Hồ sơ'),
-      ),
+      appBar: AppBar(centerTitle: false, title: const Text('Hồ sơ')),
 
       body: ListView(
         children: [
           const UserAvatar(),
           Center(child: const EditableUserDisplayName()),
-          SignOutButton(
-            auth: FirebaseAuth.instance,
-          )
+          SignOutButton(auth: FirebaseAuth.instance),
+
+          SizedBox(height: 80),
+
+          //dùng FutureBuilder để đợi có thông tin thì mới in được
+          FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final packageInfo = snapshot.data!;
+                return Center(
+                  child: Text(
+                    'IOT chăn nuôi version ${packageInfo.version} build ${packageInfo.buildNumber}',
+                    style: const TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                );
+              }
+              //return rỗng khi chưa có data
+              return SizedBox.shrink();
+            },
+          ),
         ],
       ),
     );
